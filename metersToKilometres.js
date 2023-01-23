101f510857e0f603c7ccb0ada7ee65c32193b1d5
Dimitar Malinov <122891194+DMalinov@users.noreply.github.com>
2023-01-23 18:03:05 +0200

    Add files via upload

---
+++ metersToKilometres.js
@@ -0,0 +1,6 @@
+function metersToKilometres(meters) {
+        let kilometres = meters / 1000;
+        console.log(kilometres.toFixed(2));
+    }
+
+    metersToKilometres(1852);
---
+++ sumDigits.js
@@ -0,0 +1,12 @@
+function sumDigits(num) {
+  let sum = 0;
+  let numToString = num.toString();
+
+  for (let i = 0; i < numToString.length; i++) {
+    sum += Number(numToString[i]);
+  }
+    console.log(sum);
+
+}
+
+sumDigits(245678);
